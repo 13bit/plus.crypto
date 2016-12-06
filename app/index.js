fbe5369e8dc4ab0241a10b3e82667962f5a294d6
Dmitry Ryazantsev <dsr.workk@gmail.com>
2016-12-06 18:58:20 +0200

    -added download/upload files

--- app/index.js
+++ app/index.js
@@ -1,13 +1,25 @@
-var app = angular.module('CryptoApp', ['simplemde']);
+var app = angular.module('CryptoApp', ['simplemde', 'ngFileSaver']);
 
-app.directive('custom-simplemde', [function(){
-    return {
-        restrict: 'A',
-        require: 'simplemde',
-        link: function(scope, element, attrs, simplemde) {
-            simplemde.get() // => SimpleMDE instance
-            simplemde.rerenderPreview()
+app.controller('CryptoCtrl', function ($scope, FileSaver, Blob) {
+    $scope.text = '';
+    $scope.file = null;
+
+    $scope.download = function () {
+        var data = new Blob([$scope.text], {type: 'text/plain;charset=utf-8'});
+        FileSaver.saveAs(data, 'text.txt');
+    };
+    $scope.add = function () {
+        var file = document.getElementById('file').files[0];
+        if (file) {
+            var r = new FileReader();
+            r.onloadend = function (e) {
+                var data = e.target.result;
+                $scope.text = data;
+                $scope.$apply();
+            }
+            r.readAsText(file);
         }
     }
-}]);
+
+});
 
